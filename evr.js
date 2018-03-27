889f364bf0c5c60996dd55cb28a4422c56adcf4c
d3x0r <d3x0r@no-reply.github.com>
2018-03-26 17:09:41 -0700

    Copy SRG locally

--- evr.js
+++ evr.js
@@ -14,7 +14,7 @@ makeEVR.addLocalStorage = addLocalStorage;
 makeEVR.addRemoteStorage = addRemoteStorage;
 //makeEVR.
 
-var rng = require('../salty_random_generator.js').SaltyRNG((salt) => { salt.length = 0; salt.push(Date.now()) });
+var rng = require('./salty_random_generator.js').SaltyRNG((salt) => { salt.length = 0; salt.push(Date.now()) });
 
 //const charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_";
 const charset = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g"
